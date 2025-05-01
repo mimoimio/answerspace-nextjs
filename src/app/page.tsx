31c7d780d2f57b5f7d53b096edd4116724311c90
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-02 01:46:22 +0800

    enough fo today

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,73 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { ChatBubbleIcon, HeartIcon, Share2Icon } from "@radix-ui/react-icons";
+import { Separator } from "@/components/ui/separator";
 
-const answers = [
+const posts = [
   {
-    id: 1,
-    question: "What is the capital of France?",
-    answer: "Paris",
+    post_id: 1,
+    title: "Why does it sound so good?Why does it sound so good?Why does it sound so good?Why does it sound so good?Why does it sound so good?",
+    content: "Music is so good",
+    user: "Mimoimio",
+
   },
   {
-    id: 2,
-    question: "What is the largest planet in our solar system?",
-    answer: "Jupiter",
+    post_id: 2,
+    title: "How do I make a website?",
+    content: "I don't know how to make a website. Can you help me? ",
+    user: "Mimoimio",
   },
   {
-    id: 3,
-    question: "What is the currency of Japan?",
-    answer: "Japanese yen",
+    post_id: 3,
+    title: "How do you solve this math problems?",
+    content: "What's the root of (x^2 + 1)?",
+    user: "Mimoimio",
   },
-  {
-    id: 4,
-    question: "What is the tallest mountain in the world?",
-    answer: "Mount Everest",
-  },
-  {
-    id: 5,
-    question: "What is the largest ocean in the world?",
-    answer: "Pacific Ocean",
-  },
-  {
-    id: 6,
-    question: "What is the currency of the United States?",
-    answer: "US dollar",
-  },
-  {
-    id: 7,
-    question: "What is the capital of Australia?",
-    answer: "Canberra",
-  },
-  {
-    id: 8,
-    question: "What is the currency of the United Kingdom?",
-    answer: "British pound",
-  },
-  {
-    id: 9,
-    question: "What is the largest country in the world?",
-    answer: "Russia",
-  },
-  {
-    id: 10,
-    question: "What is the currency of China?",
-    answer: "Chinese yuan",
-  },
+
 ]
 
-async function getAnswers() {
-  return answers;
+async function getPosts() {
+  return posts;
 }
 
 export default async function Home() {
+  const posts = await getPosts();
   return (
     <main className="container mx-auto">
-      <h1>AnswerSpace</h1>
+      <div className="grid grid-cols-3 gap-8">
+        {
+          posts.map((post) => (
+            <Card key={post.post_id}>
+              <CardHeader>
+                <CardTitle>{post.title}</CardTitle>
+                <CardDescription>by {post.user}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>{post.content}</p>
+                <div className="flex justify-end">
+                </div>
+              </CardContent>
+              <CardFooter className="flex flex-col w-full">
+                <Separator className="mb-4" />
+                <CardAction className="flex gap-4">
+                  <Button variant="ghost"><HeartIcon /></Button>
+                  <Button variant="ghost"><ChatBubbleIcon /></Button>
+                  <Button variant="ghost"><Share2Icon /></Button>
+
+                </CardAction>
+              </CardFooter>
+            </Card>
+          ))
+        }
+      </div>
     </main>
   );
 }
