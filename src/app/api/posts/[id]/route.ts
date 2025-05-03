fa15fe258ae04daf8ad02784b00fe5ec6eb3eaef
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-04 01:46:36 +0800

    primise?

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,8 +1,8 @@
 import { PostRepository } from "@/lib/PostRepository";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
     console.log(request);
-    const { id } = params;
+    const { id } = await params;
     const post = await PostRepository.getById(id);
     if (!post) {
         return Response.json({ error: "Post not found😭😭😭😭" }, { status: 404 });
