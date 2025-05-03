706a0219077aee148526aee31196f43a8e45a23d
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-05-04 01:39:44 +0800

    idk man

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,7 +1,8 @@
 import { PostRepository } from "@/lib/PostRepository";
 
-export async function GET({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    console.log(request);
+    const { id } = params;
     const post = await PostRepository.getById(id);
     if (!post) {
         return Response.json({ error: "Post not found😭😭😭😭" }, { status: 404 });
